9874320c9eff3d6e66e8de7cd6cd74041565e6f2
syang486 <49324020+syang486@users.noreply.github.com>
2021-04-29 08:41:00 -0400

    update static folder

---
+++ static/js/opening.js
@@ -0,0 +1,8 @@
+
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.5 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo(".landing", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".right-container", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
